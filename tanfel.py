7f50603ab2a37e7ceff381663bda93eaf25a6c08
sanyi12334 <126462899+sanyi12334@users.noreply.github.com>
2023-03-13 09:29:49 +0100

    3.feladat kész

--- tanfel.py
+++ tanfel.py
@@ -23,3 +23,15 @@ with open("beosztas.txt","r",encoding="utf-8") as fm1:
 
 print("2. feladat")
 print(f"A fájlban {len(beosztasok)} bejegyzés van.")
+
+# 3. feladat
+# Mennyi a heti óraszám az iskolában? Az eredményt írassa ki a képernyőre!
+
+def osszegzes(bok):
+    osszeg=0
+    for elem in bok:
+        osszeg+=elem["oraszam"]
+    return osszeg
+
+print("3. feladat")
+print(f"Az iskolában a heti összóraszám: {osszegzes(beosztasok)}")
